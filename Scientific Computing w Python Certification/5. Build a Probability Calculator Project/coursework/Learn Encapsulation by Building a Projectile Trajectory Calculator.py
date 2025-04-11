a787cc3bb0c8ebe16d7b6639f29d99880c18d9a5
thomas.bolstad <tombolstad45@gmail.com>
2025-04-11 15:23:59 -0500

    Add new Python file Projectile Trajectory Calculator.py

---
+++ Scientific Computing w Python Certification/5. Build a Probability Calculator Project/coursework/Learn Encapsulation by Building a Projectile Trajectory Calculator.py
@@ -0,0 +1,17 @@
+import math
+
+GRAVITATIONAL_ACCELERATION = 9.81
+PROJECTILE = "∙"
+x_axis_tick = "T"
+y_axis_tick = "⊣"
+
+class Projectile:
+    __slots__ = ('__speed', '__height', '__angle')
+
+    def __init__(self, speed, height, angle):
+        self.__speed = speed
+        self.__height = height
+        self.__angle = math.radians(angle)
+        
+    def __calculate_displacement(self):
+            pass
